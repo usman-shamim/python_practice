4366f377e014af31aa3c01b367bad5a628c65665
usman-shamim <usman2007.ap@gmail.com>
2025-10-20 17:29:59 +0500

    solve taqueria menu problem

---
+++ taqueria.py
@@ -0,0 +1,29 @@
+menu = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+
+}
+total = 0
+
+while True:
+    try:
+        item = input("Item: ")
+    except EOFError:
+        print()
+        break
+
+    item = item.title()
+
+    if item in menu:
+        print("Valid item ")
+        total += menu[item]
+        print(f"${total:.2f}")
+    else:
+        continue
